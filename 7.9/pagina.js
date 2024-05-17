2fb6ca5e020dc9e7cc23f9f411cb1c94cef41501
LOBO <salbarranmari@alumnat.iessacolomina.es>
2024-05-17 22:54:49 +0200

    Add files via upload

---
+++ 7.9/pagina.js
@@ -0,0 +1,42 @@
+function  index() {
+    document.getElementById('centro').innerHTML = document.getElementById('ind').innerHTML;
+}
+
+function info() {
+    document.getElementById('centro').innerHTML = document.getElementById('info').innerHTML;
+}
+
+
+function a() {
+    document.getElementById('centro').innerHTML = document.getElementById('a').innerHTML;
+}
+
+function b () {
+
+    document.getElementById('centro').innerHTML = document.getElementById('b').innerHTML;
+    
+}
+
+function c () {
+
+    document.getElementById('centro').innerHTML = document.getElementById('c').innerHTML;
+    
+}
+
+function d () {
+
+    document.getElementById('centro').innerHTML = document.getElementById('d').innerHTML;
+    
+}
+
+function e () {
+
+    document.getElementById('centro').innerHTML = document.getElementById('e').innerHTML;
+    
+}
+
+function f () {
+
+    document.getElementById('centro').innerHTML = document.getElementById('f').innerHTML;
+    
+}
